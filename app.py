58cd558e2336fd9c9c313c4a3c90c999369f7408
wil-h <123789183+wil-h@users.noreply.github.com>
2024-06-27 10:49:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -428,7 +428,7 @@ def uploadData():
 #---------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    #serve(app,host = '0.0.0.0',port = 5000)
+    serve(app,host = '0.0.0.0',port = 5000)
     #app.run(host='0.0.0.0')
-    app.run(debug=True,host='0.0.0.0')
+    #app.run(debug=True,host='0.0.0.0')
 
